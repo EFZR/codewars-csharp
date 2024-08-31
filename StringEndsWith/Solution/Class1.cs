538190bd91b9e50bbb857363ae117fff63331aca
efzr <emerson.zapatarivas@gmail.com>
2024-08-30 20:51:00 -0600

    StringEndsWith

---
+++ StringEndsWith/Solution/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Solution;
+
+public class Class1
+{
+    public static bool Solution(string str, string ending)
+    {
+        return str.Length > ending.Length && str[^ending.Length..] == ending;
+    }
+}

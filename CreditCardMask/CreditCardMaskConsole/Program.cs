c5122aeedc66c853ea8842606ada276adf03f31c
efzr <emerson.zapatarivas@gmail.com>
2024-11-14 06:55:25 +0000

    CreditCard ex

---
+++ CreditCardMask/CreditCardMaskConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CreditCardMaskConsole;
+
+public class CreditCard
+{
+    public static string CreditCardMask(string str)
+    {
+        // return str.Length < 4
+        //             ? str
+        //             : String.Concat(str.ToCharArray().Select((c, index) => index < str.Length - 4 ? '#' : c));
+
+        return str.Length < 4
+             ? str
+             : str.Substring(str.Length - 4).PadLeft(str.Length, '#');
+
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Testing the solution
+        Console.WriteLine(CreditCard.CreditCardMask("35616"));
+    }
+}

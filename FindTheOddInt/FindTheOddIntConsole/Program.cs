2c06f362c6c9b4d4db767795f350e2b7652c9b5b
efzr <emerson.zapatarivas@gmail.com>
2024-11-16 04:06:20 +0000

    FindIndOdd

---
+++ FindTheOddInt/FindTheOddIntConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FindTheOddInt;
+
+public static class FindTheOddIntSolution
+{
+    public static int findOddSolution(int[] seq)
+    {
+		for(int i = 0; i < seq.Length; i++)
+		{
+			var result = seq.Select((n, index) => seq[i] = n).ToList();
+			if (result.Count % 2 == 0)
+			{
+				return seq[i];
+			}
+		}
+        return -1;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        // This should return 4
+        Console.WriteLine(FindTheOddIntSolution.findOddSolution([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+    }
+}
---
+++ FindTheOddInt/FindTheOddIntTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FindTheOddIntTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
